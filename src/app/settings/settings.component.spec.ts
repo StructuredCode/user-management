7fd33ab85f54dabe28d723af2be90332ef81ce52
Matej Koprivec <matej.koprivec@outlook.com>
2024-07-09 14:43:54 +0200

    Add settings.component unit test

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,24 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SettingsComponent } from './settings.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
+  let submitButton: DebugElement;
+  const validAuthFormObj = { clientId: 'testId', clientSecret: 'testSecret' };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SettingsComponent, NoopAnimationsModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    submitButton = fixture.debugElement.nativeElement.querySelector("button[type='submit']");
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the form invalid when empty', () => {
+    expect(component.authForm.valid).toBeFalsy();
+  });
+
+  it('should have the clientId field required', () => {
+    const control = component.authForm.get('clientId');
+    control!.setValue('');
+    expect(control!.hasError('required')).toBeTruthy();
+  });
+
+  it('should have the clientSecret field required', () => {
+    const control = component.authForm.get('clientSecret');
+    control!.setValue('');
+    expect(control!.hasError('required')).toBeTruthy();
+  });
+
+  it('should have the form valid when fields are filled correctly', () => {
+    component.authForm.setValue(validAuthFormObj);
+    expect(component.authForm.valid).toBeTruthy();
+  });
+
+  it('should call onSubmit when form is valid and submitted', () => {
+    spyOn(component, 'onAuthSubmit');
+
+    component.authForm.setValue(validAuthFormObj);
+    submitButton.nativeElement.click();
+
+    expect(component.onAuthSubmit()).toHaveBeenCalled();
   });
 });
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,14 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [MatFormFieldModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, FormsModule, ReactiveFormsModule, MatCardModule, MatButtonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  private fb = inject(FormBuilder);
 
+  authForm: FormGroup = new FormGroup({
+    clientId: new FormControl<string>('', [Validators.required]),
+    clientSecret: new FormControl<string>('', [Validators.required])
+  });
 }
