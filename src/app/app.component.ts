ae99a4d5f3e74afc97fc183ad32236470bd05355
Matej Koprivec <matej.koprivec@outlook.com>
2024-07-11 05:57:16 +0200

    Implement sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
